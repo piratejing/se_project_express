da18a9a2f072fa0f2733174cc5a5660f59af66bd
Alan Shieh <piratejing41@gmail.com>
2023-06-27 21:20:47 -0700

    refactor require routes after eslint check

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const { PORT = 3001 } = process.env;
+const mongoose = require("mongoose");
 const express = require("express");
-
-const app = express();
 const routes = require("./routes");
 
-const mongoose = require("mongoose");
+const app = express();
+
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use((req, res, next) => {
