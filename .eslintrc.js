6e566d80d18200eb036c24a20d88232d3520250e
Alan Shieh <piratejing41@gmail.com>
2023-07-06 20:17:16 -0700

    installed express lint hotreload

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,23 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-
-  extends: ["eslint:recommended", "airbnb-base", "prettier"],
-  overrides: [],
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
   rules: {
-    "no-underscore-dangle": ["error", { allow: ["_id"] }],
   },
 };
