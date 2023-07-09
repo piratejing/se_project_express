5592e6662698bf6809994600e2a6a499989dbb45
Alan Shieh <piratejing41@gmail.com>
2023-07-08 20:56:58 -0700

    removed arrow function added console log message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", () => {});
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use((req, res, next) => {
   req.user = {
@@ -18,4 +18,4 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {console.log(`App is listening on port: ${PORT}`);});
